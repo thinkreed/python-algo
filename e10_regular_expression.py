297b7e842e36381f60dd02f9881bdef17ac1ac03
thinkreed <thinkreed2017@outlook.com>
2017-03-11 10:39:29 +0800

    modify e10

--- e10_regular_expression.py
+++ e10_regular_expression.py
@@ -11,6 +11,8 @@ __mtime__ = '17/3/10'
 class Solution(object):
     def isMatch(self, s, p):
         """
+        great thanks to https://discuss.leetcode.com/topic/40371/easy-dp-java-solution-with-detailed-explanation/2
+        and https://discuss.leetcode.com/topic/6183/my-concise-recursive-and-dp-solutions-with-full-explanation-in-c/2
         :type s: str
         :type p: str
         :rtype: bool
@@ -23,9 +25,10 @@ class Solution(object):
         for i in range(len(p)):
             for j in range(len(s)):
                 if p[i] == '*':
-                    dp[i + 1][j + 1] = dp[i - 1][j + 1] or dp[i][j + 1]
-                    if p[i - 1] == s[j] or p[i - 1] == '.':
-                        dp[i + 1][j + 1] |= dp[i + 1][j]
+                    if p[i - 1] != s[j] and p[i - 1] != '.':
+                        dp[i + 1][j + 1] = dp[i - 1][j + 1]
+                    else:
+                        dp[i + 1][j + 1] = dp[i][j + 1] or dp[i - 1][j + 1] or dp[i + 1][j]
                 else:
                     dp[i + 1][j + 1] = dp[i][j] and (p[i] == s[j] or p[i] == '.')
         return dp[-1][-1]
