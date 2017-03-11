4251e8a71f7692f51716d6e3bb700aea0df43912
thinkreed <thinkreed2017@outlook.com>
2017-03-11 16:17:40 +0800

    modify e6

--- e6_zigzag_string.py
+++ e6_zigzag_string.py
@@ -10,15 +10,33 @@ __mtime__ = '17/3/6'
 
 class Solution:
     def zigzag_string(self, str, n_rows):
-        str_ret = ""
-        str_len = len(str)
-        for i in range(n_rows):
-            j = i
-            while j < str_len:
-                str_ret += str[j]
-                j = (j + n_rows + 1) if (i % 2 == 0) else (j + n_rows - (i // 2 + 1))
 
-        return str_ret
+        if n_rows == 1:
+            return str
+
+        buffers = [""] * n_rows
+        str_len = len(str)
+        i = 0
+
+        while i < str_len:
+
+            for j in range(n_rows):
+
+                if i >= str_len:
+                    break
+
+                buffers[j] += str[i]
+                i += 1
+
+            for j in range(n_rows - 2, 0, -1):
+
+                if i >= str_len:
+                    break
+
+                buffers[j] += str[i]
+                i += 1
+
+        return "".join(buffers)
 
 
 if __name__ == '__main__':
