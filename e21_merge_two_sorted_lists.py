69aac59d539e20b7ee022298baa9d0455748d7c6
thinkreed <thinkreed2017@outlook.com>
2017-03-23 23:07:06 +0800

    add e21

---
+++ e21_merge_two_sorted_lists.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'thinkreed'
+__mtime__ = '2017/3/23'
+
+"""
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
