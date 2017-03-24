0691b2e698cd70fa9f149bb7b38a3fda2187d798
thinkreed <thinkreed2017@outlook.com>
2017-03-24 14:19:40 +0800

    add e22

---
+++ e22_generate_parenthesis.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'thinkreed'
+__mtime__ = '2017/3/24'
+
+"""
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        idea from https://discuss.leetcode.com/topic/17510/4-7-lines-python/2
+        """
+        def generate(p, left, right, parens=[]):
+            if left:         generate(p + '(', left-1, right)
+            if right > left: generate(p + ')', left, right-1)
+            if not right:    parens += p,
+            return parens
+        return generate('', n, n)
