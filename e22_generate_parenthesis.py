53486aa9705eade7e6101bd58645f42e8dd814a2
thinkreed <thinkreed2017@outlook.com>
2017-03-24 14:20:57 +0800

    modify e22

--- e22_generate_parenthesis.py
+++ e22_generate_parenthesis.py
@@ -7,6 +7,7 @@ __mtime__ = '2017/3/24'
 
 """
 
+
 class Solution(object):
     def generateParenthesis(self, n):
         """
@@ -14,9 +15,11 @@ class Solution(object):
         :rtype: List[str]
         idea from https://discuss.leetcode.com/topic/17510/4-7-lines-python/2
         """
+
         def generate(p, left, right, parens=[]):
-            if left:         generate(p + '(', left-1, right)
-            if right > left: generate(p + ')', left, right-1)
+            if left:         generate(p + '(', left - 1, right)
+            if right > left: generate(p + ')', left, right - 1)
             if not right:    parens += p,
             return parens
+
         return generate('', n, n)
